188c0b86f60eafea564b55cf94cab02dfebdf4c8
SkyWasabi <dakeheng@hotmail.com>
2016-05-23 15:52:22 +1000

    part 1 complete

--- app/src/main/java/com/example/dakeh/assignment3/SatelliteFragment.java
+++ app/src/main/java/com/example/dakeh/assignment3/SatelliteFragment.java
@@ -1,26 +1,46 @@
 package com.example.dakeh.assignment3;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 public class SatelliteFragment extends Fragment {
+
+    TextView textView;
+    ImageView mapview;
 
     private float lat;
     private float lon;
     private String api_key = "11KwxbBt94WW26CWimobLPhaE4AgXxxhwxywTTXU";
     private URL api_url;
-
-    public SatelliteFragment() {
-        // Required empty public constructor
-    }
-
+    private String savepath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,8 +51,12 @@ public class SatelliteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_satellite, container, false);
+//         Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_satellite, container, false);
+        mapview = (ImageView) view.findViewById(R.id.mapview);
+        textView = (TextView) view.findViewById(R.id.labeltext);
+
+        return view;
     }
 
 
@@ -40,6 +64,133 @@ public class SatelliteFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
 
+    }
+
+    public void performNASARequest(String date,Satellite satellite) {
+
+        String url = "https://api.nasa.gov/planetary/earth/imagery?lon=" + String.valueOf(satellite.getLon()) + "&lat=" + String.valueOf(satellite.getLat())
+                + "&date=" + date + "&cloud_score=True&api_key=" + satellite.getApi_key();
+
+        //new DownloadTask().execute("https://api.nasa.gov/planetary/earth/imagery?lon=150.8931239&lat=-34.424984&date=2015-05-01&cloud_score=True&api_key=11KwxbBt94WW26CWimobLPhaE4AgXxxhwxywTTXU");
+        new DownloadTask().execute(url);
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return performRequest(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Log.v("tag", result);
+            //textView.setText(result);
+            fetchImage(result);
+        }
+
+        private String performRequest(String myURL) throws  IOException {
+            InputStream in = null;
+
+            try {
+                URL url = new URL(myURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(10000);
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+
+                int response = connection.getResponseCode();
+
+                Log.v("Response code", String.valueOf(response));
+
+                in = connection.getInputStream();
+                String stringResult = parseStream(in);
+
+                return stringResult;
+            } finally {
+                if (in != null) {
+                    in.close();
+                }
+            }
+        }
+
+        public  String parseStream(InputStream stream) throws  IOException, UnsupportedEncodingException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+            StringBuilder result = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            return result.toString();
+        }
+
+        public void fetchImage(String result) {
+            try {
+                JSONObject reader = new JSONObject(result);
+
+                String imageurl = reader.getString("url");
+                String satellitedate = reader.getString("date");
+
+                textView.setText(satellitedate);
+                //Log.d("date", satellitedate);
+                new DownloadImageTask(mapview).execute(imageurl);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class DownloadImageTask extends  AsyncTask<String, Void, Bitmap> {
+        ImageView download;
+        public DownloadImageTask(ImageView download) {
+            this.download = download;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap mappic = null;
+            try {
+                InputStream in = new URL(urls[0]).openStream();
+                mappic = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            savepath = saveToInternalStorage(mappic);
+            Log.d("Path", savepath);
+
+            return mappic;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            download.setImageBitmap(result);
+        }
+    }
+
+    private String saveToInternalStorage(Bitmap bitmap) {
+        ContextWrapper cw = new ContextWrapper(getActivity().getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File mypath = new File(directory, "satpic.jpg");
+
+        FileOutputStream fos;
+
+        try {
+            fos = new FileOutputStream(mypath);
+
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            Log.d("Save", "Complete");
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return directory.getAbsolutePath();
     }
 
 }
